7cbca34771937d6a3235f6688abb1451ca47439f
gwttk <13107071+gwttk@users.noreply.github.com>
2025-09-05 10:57:31 +0800

    fix launcher info

--- src/main/java/com/github/immueggpain/bettermultiplayer/Launcher.java
+++ src/main/java/com/github/immueggpain/bettermultiplayer/Launcher.java
@@ -6,11 +6,12 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
 
-@Command(description = "Toolkit written in java.", name = "javatool", mixinStandardHelpOptions = true,
-		version = Launcher.VERSTR, subcommands = { HelpCommand.class, BMPUDPHub.class, BMPPeer.class })
+@Command(description = "use openvpn to create a virtual LAN for multiple PCs.", name = "bmp",
+		mixinStandardHelpOptions = true, version = Launcher.VERSTR,
+		subcommands = { HelpCommand.class, BMPUDPHub.class, BMPPeer.class })
 public class Launcher implements Callable<Void> {
 
-	public static final String VERSTR = "0.5.1";
+	public static final String VERSTR = "0.6.0";
 	public static final int LOCAL_PORT = 2233;
 	public static final int LOCAL_OVPN_PORT = 1194;
 
